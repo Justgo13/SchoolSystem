1a8afb58c338f9faf89a9716b18e1733a34530c5
justgo13 <jgao2299@gmail.com>
2020-08-22 20:14:30 -0400

    Added option for professor to set course taught

--- src/ProfGUI.java
+++ src/ProfGUI.java
@@ -3,9 +3,11 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
@@ -15,6 +17,7 @@ public class ProfGUI implements ActionListener {
      * JButton for teacher
      */
     private JButton updateGrade;
+    private JButton courseToTeach;
     
 	private JFrame frame;
 
@@ -41,14 +44,19 @@ public class ProfGUI implements ActionListener {
         container.add(profPanel);
 
         updateGrade = new JButton("UPDATE GRADE");
+        courseToTeach = new JButton("SET COURSE TAUGHT");
 
         updateGrade.setFocusPainted(false);
+        courseToTeach.setFocusPainted(false);
 
         updateGrade.setFont(new Font("Arial", Font.BOLD, 40));
+        courseToTeach.setFont(new Font("Arial", Font.BOLD, 40));
 
         profPanel.add(updateGrade);
+        profPanel.add(courseToTeach);
 
         updateGrade.addActionListener(this);
+        courseToTeach.addActionListener(this);
 
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.pack();
@@ -64,7 +72,24 @@ public class ProfGUI implements ActionListener {
         JButton button = (JButton) o;
         
         if (button.equals(updateGrade)) {
-        	new UpdateGradeGUI(profID);
+        	try {
+	        	// check to see if professor has any courses
+	        	MainRun.myStmt = MainRun.myConn.prepareStatement("SELECT courseTaught FROM professor WHERE professorID = ?");
+	        	MainRun.myStmt.setString(1, profID);
+	        	MainRun.myRs = MainRun.myStmt.executeQuery();
+	        	
+	        	if (MainRun.myRs.next()) {
+	        		if (MainRun.myRs.getString("courseTaught") == null) {
+	        			JOptionPane.showMessageDialog(container, "Please choose a course to teach");
+	        		} else {
+	        			new UpdateGradeGUI(profID);
+	        		}
+	        	} 
+        	} catch (SQLException e1) {
+        		e1.getStackTrace();
+        	}
+        } else if (button.equals(courseToTeach)) {
+        	new CourseToTeach(profID);
         }
     } 
 }
